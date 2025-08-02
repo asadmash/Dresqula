0cd6b3a87b87036745225b81214852ffe5c66d4b
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 22:18:57 +0600

    style mobile nav

--- src/app/components/MobNavbar.jsx
+++ src/app/components/MobNavbar.jsx
@@ -7,21 +7,22 @@ import {AiOutlineHome, AiOutlineAppstore} from 'react-icons/ai';
 
 const MobNavbar = () => {
   return (
-    <div>
-    <div>
+    <div className='lg:hidden fixed bottom-0 w-full bg-white left-[50%] -translate-x-[50%] max-w-[500px] px-8'>
+    <div className='flex justify-between text-[28px] py-2'>
         <IoMenuOutline/>
-        <div>
+        <div className='relative'>
             <HiShoppingBag/>
-            <div>0</div>
+            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">0</div>
         </div>
-    </div>
+  
 
     <AiOutlineHome/>
-    <div>
+    <div className='relative'>
         <FiHeart/>
-        <div>0</div>
+        <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">0</div>
     </div>
     <AiOutlineAppstore/>
+      </div>
     </div>
   )
 }
