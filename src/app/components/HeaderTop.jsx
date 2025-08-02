36316468b419738c5be02966b977b7e5010e811d
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 18:59:59 +0600

    style header

--- src/app/components/HeaderTop.jsx
+++ src/app/components/HeaderTop.jsx
@@ -4,9 +4,9 @@ import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 const HeaderTop = () => {
   return (
     <div className="border-b border-gray-200 hidden sm:block">
-      <div className="container py-4">
+      <div className="@container py-4 px-20">
         <div className="flex justify-between items-center ">
-          <div className="">
+          <div className="hidden lg:flex gap-1">
             <div className="header_top_icon_wrapper">
               <BsFacebook />
             </div>
