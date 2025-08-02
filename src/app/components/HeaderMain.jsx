39c4cadc913d4a2206969b0f5fc39cdcf8954d58
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 20:18:00 +0600

    style search field

--- src/app/components/HeaderMain.jsx
+++ src/app/components/HeaderMain.jsx
@@ -8,13 +8,12 @@ const HeaderMain = () => {
     <div className="border-b border-gray-200 py-6 @container">
       <div className="container-inner sm:flex justify-between items-center">
         <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackfish">Sick</div>
-        <div className="flex">
-          <input type="text" placeholder="Enter any product name..." />
-          <BsSearch size={20} />
+        <div className="w-full sm:w-[300px] md:w-[70%] relative">
+          <input type="text" placeholder="Enter any product name..." className="border-gray-200 border p-2 px-4 rounded-lg w-full "/>
+          <BsSearch size={20} className="absolute right-0 top-0 mr-3 mt-3 text-gray-400" />
         </div>
         <div>
           <BsPerson />
-
           <div>
             <FiHeart />
             <div>0</div>
