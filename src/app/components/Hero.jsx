46cdb03a8f34c2029e1c738fa4abc7ab33432ace
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 22:39:46 +0600

    carousel cofigure

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,6 +1,39 @@
 import React from 'react'
 
 const Hero = () => {
+
+    let settings = {
+  dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+    };
+
+    const slideData = [
+        {
+            id: 0,
+            img: "/banner-1.jpg",
+            title: 'Trending Item',
+            mainTitle: "WOMEN'S LATEST FASHION SALE",
+            price: "$20",
+        }, 
+{
+    id: 1,
+    img: "/banner-2.jpg",
+    title: "Trending Accessories",
+    mainTitle: "MODERN SUNGLASSES",
+    price: "$15",
+},
+{
+    id: 2,
+    img: "/banner-3.jpg",
+    title: "Sale Offer",
+    mainTitle: "NEW FASHION SUMMER SALE",
+    price: "$30",
+},
+    ]
+
   return (
     <div>Hero</div>
   )
