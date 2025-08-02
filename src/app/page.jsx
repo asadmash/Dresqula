44879d68b0fded476dd43c1a3fc4a33032782a19
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-03 01:50:47 +0600

    add product data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import NewProducts from "./components/NewProducts";
 
 export default function Home() {
   return (
    <div>
     <Hero/>
+    <NewProducts/>
    </div>
   );
 }
