75ce7e60de0715cf401d5b3697bbe23cbd9c1eed
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 20:30:57 +0600

    header main style complete

--- src/app/components/HeaderMain.jsx
+++ src/app/components/HeaderMain.jsx
@@ -12,14 +12,15 @@ const HeaderMain = () => {
           <input type="text" placeholder="Enter any product name..." className="border-gray-200 border p-2 px-4 rounded-lg w-full "/>
           <BsSearch size={20} className="absolute right-0 top-0 mr-3 mt-3 text-gray-400" />
         </div>
-        <div>
+        <div className="hidden lg:flex text-gray-500 text-[30px] gap-4">
           <BsPerson />
-          <div>
+          <div className="relative">
             <FiHeart />
-            <div>0</div>
+            <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">0</div>
           </div>
-          <div>
+          <div className="relative">
             <HiOutlineShoppingBag />
+             <div className="bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text-[12px] text-white grid place-items-center translate-x-1 -translate-y-1">0</div>
           </div>
         </div>
       </div>
