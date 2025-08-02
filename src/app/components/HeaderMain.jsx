3f6ce88bcaa9426113e2f4705c25a1a1723af7d2
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 19:55:33 +0600

    structure header main

--- src/app/components/HeaderMain.jsx
+++ src/app/components/HeaderMain.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import { BsSearch } from 'react-icons/bs'
+import React from "react";
+import { BsPerson, BsSearch } from "react-icons/bs";
+import { FiHeart } from "react-icons/fi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 
 const HeaderMain = () => {
   return (
     <div>
+      <div>
+        <div>Sick</div>
         <div>
-            <div>Sick</div>
-            <div>
-                <input type="text" placeholder='Enter any product name...' />
-                <BsSearch/>
-            </div>
+          <input type="text" placeholder="Enter any product name..." />
+          <BsSearch />
         </div>
-    </div>
-  )
-}
+        <div>
+          <BsPerson />
 
-export default HeaderMain
+          <div>
+            <FiHeart />
+            <div>0</div>
+          </div>
+          <div>
+            <HiOutlineShoppingBag />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderMain;
