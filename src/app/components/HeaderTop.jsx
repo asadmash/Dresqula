e6a0abe67069884f2fc7f2e9fc7418fda01107c8
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-02 17:39:40 +0600

    add header icons

--- src/app/components/HeaderTop.jsx
+++ src/app/components/HeaderTop.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from "react";
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 
 const HeaderTop = () => {
   return (
     <div>
       <div>
         <div>
-          <div></div>
+          <div>
+            <div>
+              <BsFacebook />
+            </div>
+            <div>
+              <BsTwitter />
+            </div>
+             <div>
+              <BsInstagram />
+            </div>
+            <div>
+              <BsLinkedin/>
+            </div>
+          </div>
+
+          <div>
+            
+          </div>
+          
+          <div>
+           
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderTop
+export default HeaderTop;
