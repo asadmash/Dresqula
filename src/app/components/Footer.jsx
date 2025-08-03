f6c46e842dd03fe20eb80bed1ecf53a942fd0b62
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-03 13:43:57 +0600

    update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-blackfish text-gray-500 text-center py-4 pb-16 lg:pb-4">
-      Copywrite &copy; sick <br /> All Right Reserved
+      Copywrite &copy; Dresqula <br /> All Right Reserved
     </div>
   );
 };
