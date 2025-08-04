a1f201a3bca5ecf540bbf57ea5dd932963be5fb3
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-04 12:16:38 +0600

    create useEffect and fetch data to the page

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,12 +1,28 @@
 'use client';
-import { useParams } from 'next/navigation';
-import React, { useState } from 'react';
+import { notFound, useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 import {fetchProductById} from '@lib/api/products';
 
 const ProductPage = () => {
     const {id} = useParams();
     const [product, setProduct] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if(!id) return;
+
+        const fetchData = async() => {
+            try {
+                const data = await fetchProductById(id);
+                setProduct(data);
+            } catch (error) {
+                console.error('Failed to fetch product:', error);
+                notFound();
+            } finally{
+                setLoading(false);
+            }
+        }
+    })
   return (
     <div>page</div>
   )
