0b2e06fbc633565841abf8ff1748e59959bca097
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-04 12:06:35 +0600

    state & imports for details page

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+'use client';
+import { useParams } from 'next/navigation';
+import React, { useState } from 'react';
+import {fetchProductById} from '@lib/api/products';
 
 const ProductPage = () => {
+    const {id} = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
   return (
     <div>page</div>
   )
