789e52717ea789686928d52043f7f9152945c96f
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-04 12:11:37 +0600

    create a fetch function to featch product by id

--- src/lib/api/products.js
+++ src/lib/api/products.js
@@ -15,3 +15,22 @@ try {
     return error;
 }
 }
+
+
+
+export async function fetchProductsById(){
+    try {
+        // Get the response
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+        // Throw error
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+        // parse and return the data
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fetch Failed:', error);
+        return error;
+    }
+}
