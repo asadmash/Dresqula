e36962d511592773461dfa4c4cf419a32e130562
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-08-08 13:41:08 +0600

    minor update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-blackfish text-gray-500 text-center py-4 pb-16 lg:pb-4 w-full">
-      Copywrite &copy; Dresqula.  All Rights Reserved
+      Copywrite &copy; Dresqula. All Rights Reserved <br />
+      Developed by{" "}
+      <a
+        className="text-gray-200"
+        href="https://asadmashuq.pages.dev/"
+        target="_blank"
+      >
+        asad.mashuq
+      </a>
     </div>
   );
 };
